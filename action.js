558a702ea5fc93cb3d9525cf22247710862950fa
Zach G <zabros20@gmail.com>
2019-03-30 21:01:10 -0600

    Commit 3, added new game button

--- action.js
+++ action.js
@@ -2,7 +2,6 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         let data = JSON.parse(xhttp.responseText);
-        console.log(data);
         for (let i = 0; i < data.courses.length; i++) {
             $("#courseSelect").append(`<option value='${data.courses[i].id}'>${data.courses[i].name}</option>`);
         }
@@ -10,13 +9,13 @@ xhttp.onreadystatechange = function () {
 };
 xhttp.open("GET", "https://golf-courses-api.herokuapp.com/courses", true);
 xhttp.send();
+
 function updateSelectDifficulty() {
-    let courseVal = document.getElementById("courseSelect").value;
+    let courseVal = $("#courseSelect").val();
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             let data = JSON.parse(xhttp.responseText);
-            console.log(data);
             $("#difficulty").html('');
             for (let i = 0; i < data.data.holes[0].teeBoxes.length; i++) {
                 $("#difficulty").append(`<option value='${data.data.holes[0].teeBoxes[i].teeTypeId}'>${data.data.holes[0].teeBoxes[i].teeType}</option>`)
@@ -24,5 +23,20 @@ function updateSelectDifficulty() {
         }
     };
     xhttp.open("GET", `https://golf-courses-api.herokuapp.com/courses/${courseVal}`, true);
+    xhttp.send();
+}
+
+function newGame() {
+    let course = $("#courseSelect").val();
+    let difficulty = $("#difficulty").val();
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let data = JSON.parse(xhttp.responseText);
+            console.log(data);
+        }
+    };
+    xhttp.open("GET", `https://golf-courses-api.herokuapp.com/courses/${course}`, true);
     xhttp.send();
 }
