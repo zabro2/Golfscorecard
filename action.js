b9ba487152e4aa998baf01034f042bbdb8b1ac05
Zach G <zabros20@gmail.com>
2019-04-01 10:21:29 -0600

    Commit 7, changed the names to input that added them to an array

--- action.js
+++ action.js
@@ -1,3 +1,6 @@
+let playerCount = 0;
+let players = []
+
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
@@ -34,18 +37,21 @@ function newGame() {
         if (this.readyState == 4 && this.status == 200) {
             let data = JSON.parse(xhttp.responseText);
             $(".infoSpot").html(`<div class='holeNumberRow'></div>
+            <div class='yardRow'></div>
             <div class='handicapRow'></div>
-            <div class='parRow'></div>
-            <div class='yardRow'></div>`);
-            $('.infoSpotNamesHolder').html(`<div class='infoSpotNames' id='holeNumberName'>Hole</div>
+            <div class='parRow'></div>`);
+            $(".totalsCol").html(`<div>`)
+            $('.infoSpotNamesHolder').html(`<div class='infoSpotNames' id='holeNumberName'></div>
+            <div class='infoSpotNames' id='yardName'>Yards</div>
             <div class='infoSpotNames' id='handicapName'>Handicap</div>
-            <div class='infoSpotNames' id='parName'>Par</div>
-            <div class='infoSpotNames' id='yardName'>Yards</div>`);
+            <div class='infoSpotNames' id='parName'>Par</div>`);
+
             for (let i = 0; i < data.data.holeCount; i++) {
                 $(".holeNumberRow").append(`<div class='holeNumber' id='holeNumber${i}'>${i+1}</div>`);
+                $(".yardRow").append(`<div class='yard' id='yard${i}'>${data.data.holes[i].teeBoxes[difficulty-1].yards}</div>`);
                 $(".handicapRow").append(`<div class='handicap' id='handicap${i}'>${data.data.holes[i].teeBoxes[difficulty-1].hcp}</div>`);
                 $(".parRow").append(`<div class='par' id='par${i}'>${data.data.holes[i].teeBoxes[difficulty-1].par}</div>`);
-                $(".yardRow").append(`<div class='yard' id='yard${i}'>${data.data.holes[i].teeBoxes[difficulty-1].yards}</div>`);
+
             }
         }
     };
@@ -53,24 +59,39 @@ function newGame() {
     xhttp.send();
 }
 
-let playerCount = 0;
+function checkNameKey(event) {
+    switch(event.keyCode) {
+        case 13:
+        newPlayer();
+        $("#playerNameInput").focus();
+        break;
+    }
+}
 
 function newPlayer() {
     let course = $("#courseSelect").val();
-
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
+        let nameInput = $("#playerNameInput")
         if (this.readyState == 4 && this.status == 200) {
             let data = JSON.parse(xhttp.responseText);
             console.log(data);
-            if (playerCount < 4) {
-                $(".names").append(`<div class='name' id='${playerCount}' contenteditable='true'>Player</div>`);
+            if (playerCount >= 4) {
+                alert('Player limit has been reached');
+                nameInput.val('');
+            } else if (players.includes(nameInput.val())) {
+                alert('That name is alredy taken');
+                nameInput.val('');
+            } else {
+                $(".names").append(`<div class='name' id='player${playerCount}'>${nameInput.val()}</div>`);
+                players.push(nameInput.val());
                 $(".bRight").append(`<div class='holes' id='holes${playerCount}'></div>`);
                 for (let i = 0; i < data.data.holeCount; i++) {
-                    $("#holes" + playerCount).append(`<div id='hole${[i]}' class='hole'>
-                <div contenteditable='true'>0</div>
+                    $("#holes" + playerCount).append(`<div id='holeHolder${i}' class='hole'>
+                <input class='holeScore' id='hole${i}' type='number' placeholder='0'>
                 </div>`);
                 }
+                nameInput.val('');
                 playerCount++;
             }
         }
